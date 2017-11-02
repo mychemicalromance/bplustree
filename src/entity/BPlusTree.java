2886d46270734c9163f9f0f99049a93606b75fcb
yangzhec <yangzhec@dcits.com>
2017-11-02 17:56:03 +0800

    aaa

--- src/entity/BPlusTree.java
+++ src/entity/BPlusTree.java
@@ -51,7 +51,42 @@ public class BPlusTree {
 				root.getNodeData().insertSorted(middle);
 			}
 		}else{
-			System.out.println("value to big!");
+			//这种情况下树至少有三个节点，此种情况下要考虑叶子分裂的问题，并且有可能导致根分裂，
+			//最复杂的情况是整个树增加一层，需要递归调用
+			//首先找到需要将值插入的叶子节点，插入之后判断是否需要分裂，如果需要分裂，分裂完成后再将中间值插入父节点，以此类推
+			Node currentNode = root;
+			while(true){
+				if(currentNode.isLeaf()){
+					break;
+				}
+				List<String> cnDatas = currentNode.getNodeData();
+				int choseChild = 0;
+				for(int i=0;i<=cnDatas.size();i++){
+					if(i==0){
+						String upper = cnDatas.get(0);
+						if(s.compareTo(upper)<0){
+							choseChild = i;
+							break;
+						}
+					}else if(i==cnDatas.size()){
+						String lower = cnDatas.get(i-1);
+						if(s.compareTo(lower)>=0){
+							choseChild = i;
+							break;
+						}
+					}else{
+						String lower = cnDatas.get(i-1);
+						String upper = cnDatas.get(i);
+						if(s.compareTo(lower)>=0 && s.compareTo(upper)<0){
+							//刚好位于整个区间
+							choseChild = i;
+							break;
+						}
+					}
+				}
+				currentNode = currentNode.getChildren().get(choseChild);
+			}
+			System.out.println("chose leaf:"+currentNode.getNodeData());
 		}
 	}
 	
